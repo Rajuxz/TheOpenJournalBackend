00fd1a28da74aa1097a0eb896dbf6b3ad72edde2
Rajuxz <rajankhanalxz@gmail.com>
2024-12-12 22:12:22 +0545

    repo: Tag Repository

---
+++ TheOpenJournal/Repository/Implementations/TagRepository.cs
@@ -0,0 +1,15 @@
+﻿using TheOpenJournal.Data;
+using TheOpenJournal.Models.Domain;
+using TheOpenJournal.Repository.Interfaces;
+
+namespace TheOpenJournal.Repository.Implementations
+{
+    public class TagRepository:Repository<Tag>,ITagRepository
+    {
+        internal readonly ApplicationDbContext _context;
+        public TagRepository(ApplicationDbContext context):base(context)
+        {
+            _context = context;            
+        }
+    }
+}

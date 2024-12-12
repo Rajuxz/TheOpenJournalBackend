8bd7143ec9881879ecd9cec573367281337db0f8
Rajuxz <rajankhanalxz@gmail.com>
2024-12-12 22:11:37 +0545

    repo: Category Repository

---
+++ TheOpenJournal/Repository/Implementations/CategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using TheOpenJournal.Data;
+using TheOpenJournal.Models.Domain;
+using TheOpenJournal.Repository.Interfaces;
+
+namespace TheOpenJournal.Repository.Implementations
+{
+    public class CategoryRepository:Repository<Category>,ICategoryRepository
+    {
+        internal readonly ApplicationDbContext _context;
+        public CategoryRepository(ApplicationDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ TheOpenJournal/Repository/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,8 @@
+﻿using TheOpenJournal.Models.Domain;
+
+namespace TheOpenJournal.Repository.Interfaces
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+    }
+}

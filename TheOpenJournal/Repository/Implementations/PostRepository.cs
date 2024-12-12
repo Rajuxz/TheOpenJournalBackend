dc71d2f6c0066925367933d9b3d5c435f6564583
Rajuxz <rajankhanalxz@gmail.com>
2024-12-12 22:11:54 +0545

    implementation: PostRepository

---
+++ TheOpenJournal/Repository/Implementations/PostRepository.cs
@@ -0,0 +1,16 @@
+﻿using TheOpenJournal.Data;
+using TheOpenJournal.Models.Domain;
+using TheOpenJournal.Repository.Interfaces;
+
+namespace TheOpenJournal.Repository.Implementations
+{
+    public class PostRepository:Repository<Post>,IPostRepository
+    {
+        internal readonly ApplicationDbContext _context;
+
+        public PostRepository(ApplicationDbContext context):base(context) 
+        {
+            _context = context;            
+        }
+    }
+}

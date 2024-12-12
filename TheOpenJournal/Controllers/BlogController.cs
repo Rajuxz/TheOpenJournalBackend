40c4d1467d39fd75352e8d7e64a70711916fd523
Rajuxz <rajankhanalxz@gmail.com>
2024-12-12 22:10:59 +0545

    controller: api endpoint for blogs

---
+++ TheOpenJournal/Controllers/BlogController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace TheOpenJournal.Controllers
+{
+    [Route("api/v1/blog")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult AddBlog()
+        {
+            try
+            {
+                return Ok(new { message = "Post Added Successfully." });
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }           
+    }
+}

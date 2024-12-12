8923331bd4e2dc05e9c505c949bd160fa32389c0
Rajuxz <rajankhanalxz@gmail.com>
2024-12-12 22:11:18 +0545

    data transfer object for post

---
+++ TheOpenJournal/Models/DTOs/PostDTO.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TheOpenJournal.Models.Domain;
+
+namespace TheOpenJournal.Models.DTOs
+{
+    public class PostDTO
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public string Slug { get; set; }
+        public string? FeaturedImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        [Required]
+        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
+        [Required] 
+        [ForeignKey(nameof(Category))]
+        public Guid CategoryId { get; set; }
+    }
+}

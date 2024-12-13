4a268bae5e9bdec20bdc0d1440adf4cfcde340cb
Rajuxz <rajankhanalxz@gmail.com>
2024-12-13 22:43:53 +0545

    change: unique slug

--- TheOpenJournal/Migrations/20241212170553_slugUnique.Designer.cs
+++ TheOpenJournal/Migrations/20241212170553_slugUnique.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TheOpenJournal.Data;
@@ -11,9 +12,10 @@ using TheOpenJournal.Data;
 namespace TheOpenJournal.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241212170553_slugUnique")]
+    partial class slugUnique
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

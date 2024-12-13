8edbf6ac55db875f93ee13e679eb6b25834b86a3
Rajuxz <rajankhanalxz@gmail.com>
2024-12-13 22:44:04 +0545

    add: mapper for category

---
+++ TheOpenJournal/Mapper/CategoryMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TheOpenJournal.Models.Domain;
+using TheOpenJournal.Models.DTOs;
+
+namespace TheOpenJournal.Mapper
+{
+    public class CategoryMapper:Profile
+    {
+        public CategoryMapper()
+        {
+            //from category to categoryDTo
+            CreateMap<Category, CategoryDTO>();
+
+            //from CategoryDto to category
+            CreateMap<CategoryDTO, Category>();
+
+        }
+    }
+}

831266fee3e371466e9fec77d295afae44df6023
Rajuxz <rajankhanalxz@gmail.com>
2024-12-13 22:45:09 +0545

    mod: id nullable in dto

---
+++ TheOpenJournal/Models/DTOs/CategoryDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TheOpenJournal.Models.DTOs
+{
+    public class CategoryDTO
+    {  
+        public Guid? Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+    }
+}

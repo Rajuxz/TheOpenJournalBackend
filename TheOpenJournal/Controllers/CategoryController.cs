d92ecb6b8f7fb2696b4263daf228fbc62a5f6dd5
Rajuxz <rajankhanalxz@gmail.com>
2024-12-13 22:43:10 +0545

    add: create and get categories endpoint

---
+++ TheOpenJournal/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TheOpenJournal.Models.DTOs;
+using TheOpenJournal.Services.Interfaces;
+
+namespace TheOpenJournal.Controllers
+{
+    [ApiController]
+    [Route("api/v1/category")]
+    [Authorize]
+    public class CategoryController : ControllerBase
+    {
+        internal readonly ICategoryServices _categoryServices;
+        public CategoryController(ICategoryServices categoryServices)
+        { 
+            _categoryServices = categoryServices;
+        }
+        [HttpPost("create-categories")]
+        public  async Task<IActionResult> CreateCategories(CategoryDTO categoryDto)
+        {
+            if (ModelState.IsValid)
+            {
+               var response = await _categoryServices.CreateCategoryAsync(categoryDto);
+               return response;
+            }
+            else
+            {
+                return BadRequest(new {message="Model is not valid."});
+            }
+        }
+        [HttpGet("get-categories")]
+        public async Task<IActionResult> GetCategories()
+        {
+            var response = await _categoryServices.GetCategoriesAsync();
+            return Ok(response);
+        }
+        
+    }
+}

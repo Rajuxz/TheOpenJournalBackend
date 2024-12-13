3798242130b3f2f93cfcf62a99d3e11610a815fb
Rajuxz <rajankhanalxz@gmail.com>
2024-12-13 22:46:06 +0545

    mod: made methods async

---
+++ TheOpenJournal/Services/Interfaces/ICategoryServices.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TheOpenJournal.Models.DTOs;
+
+namespace TheOpenJournal.Services.Interfaces
+{
+    public interface ICategoryServices
+    {
+        public Task<ActionResult> CreateCategoryAsync(CategoryDTO categoryDto);
+        public Task<ActionResult> UpdateCategoryAsync(Guid categoryId,CategoryDTO categoryDto);
+        public Task<ActionResult> DeleteCategoryAsync(Guid categoryId);
+        public Task<ActionResult<IQueryable<CategoryDTO>>> GetCategoriesAsync();
+    }
+}

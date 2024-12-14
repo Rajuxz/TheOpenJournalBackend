1e4b9d6ec7d352493b82bd8983ff3b806b04c8da
Rajuxz <rajankhanalxz@gmail.com>
2024-12-14 22:19:49 +0545

    add; updateDto

--- TheOpenJournal/Models/DTOs/CategoryDTO.cs
+++ TheOpenJournal/Models/DTOs/CategoryDTO.cs
@@ -4,7 +4,12 @@ namespace TheOpenJournal.Models.DTOs
 {
     public class CategoryDTO
     {  
-        public Guid? Id { get; set; } = Guid.NewGuid();
+        public Guid? Id { get; set; } 
+        public string Name { get; set; }
+    }
+    public class UpdateCategoryDTO
+    {
+        public Guid Id { get; set; }
         public string Name { get; set; }
     }
 }

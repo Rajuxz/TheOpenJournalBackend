17299cb77d7c537d80be017cb1ef7d917aecd823
Rajuxz <rajankhanalxz@gmail.com>
2024-12-14 22:21:27 +0545

    for posts

---
+++ TheOpenJournal/Services/Implementation/PostService.cs
@@ -0,0 +1,19 @@
+﻿using TheOpenJournal.Models.DTOs;
+using TheOpenJournal.Repository.Interfaces;
+using TheOpenJournal.Services.Interfaces;
+
+namespace TheOpenJournal.Services.Implementation
+{
+    public class PostService:IPostService
+    {
+        private readonly IPostRepository _repository;
+        public PostService(IPostRepository repository){ 
+            _repository = repository;
+        }
+
+        public async Task<bool> AddPostAsync(PostDTO postDto)
+        {
+            return false;
+        }
+    }
+}

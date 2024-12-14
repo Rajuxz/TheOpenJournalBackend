222bb11db278407a239ce17147f0ad79a72d9afc
Rajuxz <rajankhanalxz@gmail.com>
2024-12-14 22:22:06 +0545

    add: postservice interface

---
+++ TheOpenJournal/Services/Interfaces/IPostService.cs
@@ -0,0 +1,9 @@
+﻿using TheOpenJournal.Models.DTOs;
+
+namespace TheOpenJournal.Services.Interfaces
+{
+    public interface IPostService
+    {
+        Task<bool> AddPostAsync(PostDTO postDto);
+    }
+}

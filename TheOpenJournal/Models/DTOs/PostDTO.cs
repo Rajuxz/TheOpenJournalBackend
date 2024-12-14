e03a4abb104ebeb47f394fdf24df163aa059bea9
Rajuxz <rajankhanalxz@gmail.com>
2024-12-14 22:20:05 +0545

    rename: string to IFormFile

--- TheOpenJournal/Models/DTOs/PostDTO.cs
+++ TheOpenJournal/Models/DTOs/PostDTO.cs
@@ -11,15 +11,11 @@ namespace TheOpenJournal.Models.DTOs
         public string Title { get; set; }
         [Required]
         public string Content { get; set; }
-
         [Required]
         public string Slug { get; set; }
-        public string? FeaturedImageUrl { get; set; }
+        public IFormFile? FeaturedImage { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
         public DateTime? UpdatedAt { get; set; }
-        public ICollection<Tag> Tags { get; set; } = new List<Tag>();
-        [Required] 
-        [ForeignKey(nameof(Category))]
-        public Guid CategoryId { get; set; }
+        List<Guid> CategoryId { get; set; } = new List<Guid>();
     }
 }

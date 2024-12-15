f10b11f4f018cc3fd7f9db0833a52c0ec4ef10e3
Rajuxz <rajankhanalxz@gmail.com>
2024-12-15 22:54:09 +0545

    add: mapper for post

--- TheOpenJournal/Mapper/PostMapper.cs
+++ TheOpenJournal/Mapper/PostMapper.cs
@@ -1,8 +1,24 @@
 ﻿using AutoMapper;
+using TheOpenJournal.Models.Domain;
+using TheOpenJournal.Models.DTOs;
 
 namespace TheOpenJournal.Mapper
 {
     public class PostMapper:Profile
     {
+        public PostMapper() {
+            CreateMap<PostDTO, Post>()
+                .ForMember(dest=>dest.Id,opt=>opt.Ignore())
+                .ForMember(dest => dest.FeaturedImageUrl, opt => opt.Ignore())
+                .ForMember(dest => dest.Categories, opt => opt.Ignore())
+                .ForMember(dest => dest.UniqueViewCount, opt => opt.Ignore())
+                .ForMember(dest => dest.LikeCount, opt => opt.Ignore())
+                .ForMember(dest => dest.CommentCount, opt => opt.Ignore())
+                .ForMember(dest=>dest.User,opt=>opt.Ignore())
+                .ForMember(dest => dest.UserId, opt => opt.Ignore())
+                .ForMember(dest => dest.CreatedAt,opt=>opt.Ignore())
+                .ForMember(dest =>  dest.UpdatedAt,opt=>opt.Ignore())
+                ;
+        }
     }
 }

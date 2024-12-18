2488acb891e05d5d44555d5fe2a0d8398fca5f4a
Rajuxz <rajankhanalxz@gmail.com>
2024-12-18 23:05:46 +0545

    signature for method

--- TheOpenJournal/Repository/Interfaces/IUserRepository.cs
+++ TheOpenJournal/Repository/Interfaces/IUserRepository.cs
@@ -5,6 +5,6 @@ namespace TheOpenJournal.Repository.Interfaces
     public interface IUserRepository:IRepository<UserModel>
     {
        Task<UserModel> GetEmailAsync(string email);
-
+       Task<string> GetIdByEmailAsync(string email);
     }
 }

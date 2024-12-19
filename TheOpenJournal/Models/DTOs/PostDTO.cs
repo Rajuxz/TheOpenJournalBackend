6d3a78c4d9ece4f7036c6c1163d5322da72dcedf
Rajuxz <rajankhanalxz@gmail.com>
2024-12-19 22:38:18 +0545

    add: GetPostsDTO

--- TheOpenJournal/Models/DTOs/PostDTO.cs
+++ TheOpenJournal/Models/DTOs/PostDTO.cs
@@ -17,4 +17,20 @@ namespace TheOpenJournal.Models.DTOs
         public List<Guid> CategoryId { get; set; } 
         public string? User { get; set; }
     }
+
+    // PostDTO for Retrieving Posts.
+    class GetPostDTO
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Slug { get; set; }
+        public string? FeaturedImageUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+        public int LikeCount { get; set; } = 0;
+        public int CommentCount { get; set; } = 0;
+        public int UniqueViewCount { get; set; } = 0;
+        public string UserId { get; set; }
+    }
 }

ac5bfa35584c7b864acfab91d60670a23052a327
Rajuxz <rajankhanalxz@gmail.com>
2024-12-23 21:37:54 +0545

    Dto for update

---
+++ TheOpenJournal/Models/DTOs/UpdatePostDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace TheOpenJournal.Models.DTOs
+{
+    public class UpdatePostDTO
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string Slug { get; set; }
+        public string? FeaturedImageUrl { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public ICollection<CategoryDTO> Categories { get; set; } = new List<CategoryDTO>();
+        public int LikeCount { get; set; } = 0;
+        public int CommentCount { get; set; } = 0;
+        public int UniqueViewCount { get; set; } = 0;
+        public string UserId { get; set; }
+    }
+}

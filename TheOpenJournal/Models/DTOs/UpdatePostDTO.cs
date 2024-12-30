8e6d22206580f7fb499d3979847d810cbeab635e
Rajuxz <rajankhanalxz@gmail.com>
2024-12-30 23:03:18 +0545

    add: updatePostDTO for getting Ids only

--- TheOpenJournal/Models/DTOs/UpdatePostDTO.cs
+++ TheOpenJournal/Models/DTOs/UpdatePostDTO.cs
@@ -3,15 +3,21 @@
     public class UpdatePostDTO
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-        public string Slug { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+        public string? Slug { get; set; }
         public string? FeaturedImageUrl { get; set; }
         public DateTime? UpdatedAt { get; set; }
-        public ICollection<CategoryDTO> Categories { get; set; } = new List<CategoryDTO>();
-        public int LikeCount { get; set; } = 0;
-        public int CommentCount { get; set; } = 0;
-        public int UniqueViewCount { get; set; } = 0;
-        public string UserId { get; set; }
+        public ICollection<UpdatePostCategoryDto>? Categories { get; set; } = new List<UpdatePostCategoryDto>();
+        public int? LikeCount { get; set; } = 0;
+        public int? CommentCount { get; set; } = 0;
+        public int? UniqueViewCount { get; set; } = 0;
+        public string? UserId { get; set; }
+    }
+
+    // To Update Categories during Post Update
+    public class UpdatePostCategoryDto
+    {
+        public Guid Id { get; set; }
     }
 }

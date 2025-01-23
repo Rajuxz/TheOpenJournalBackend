49e021318749ebdfc0699384ee4d2fcefb45ecbc
Rajuxz <rajankhanalxz@gmail.com>
2025-01-23 23:15:07 +0545

    add: pager

---
+++ TheOpenJournal/Models/Domain/Pager.cs
@@ -0,0 +1,11 @@
+﻿using AutoMapper.Configuration.Conventions;
+
+namespace TheOpenJournal.Models.Domain
+{
+    public class PaginationParameters
+    {
+        //By default, pagesize is 5 and page number is 1
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
+    }
+}

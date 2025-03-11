cad23b0bd33433591adb19c2c0fe54faa8059149
Rajuxz <rajankhanalxz@gmail.com>
2025-03-11 23:27:26 +0545

    new: like controller

---
+++ TheOpenJournal/Controllers/LikeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheOpenJournal.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/v1/likes")]
+    public class LikeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult> GetLikes()
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<ActionResult> AddLikes([FromForm]Guid id)
+        {
+            return Ok(id);
+        }
+        [HttpPatch]
+        public async Task<ActionResult> UpdateLikes([FromForm] Guid id)
+        {
+            return Ok(id);
+        }
+        
+    }
+}

0af3dafc19b707713bb9b0623f611c48b024ab0b
Rajuxz <rajankhanalxz@gmail.com>
2025-03-11 23:28:53 +0545

    new: Service for Like

---
+++ TheOpenJournal/Services/Implementation/LikeService.cs
@@ -0,0 +1,12 @@
+﻿using TheOpenJournal.Services.Interfaces;
+
+namespace TheOpenJournal.Services.Implementation
+{
+    public class LikeService : ILikeService
+    {
+        public Task<bool> LikePost(string userId, string postId)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}

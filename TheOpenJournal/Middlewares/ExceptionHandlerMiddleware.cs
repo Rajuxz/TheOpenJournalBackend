12025b311ff6c2a1c6b66b5bce9c71e91973a6d1
Rajuxz <rajankhanalxz@gmail.com>
2025-04-12 23:46:06 +0545

    add: exception middleware

---
+++ TheOpenJournal/Middlewares/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace TheOpenJournal.Middlewares
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly ILogger<ExceptionHandlerMiddleware> _logger;
+        private readonly RequestDelegate _next;
+
+        public ExceptionHandlerMiddleware(ILogger<ExceptionHandlerMiddleware> logger, RequestDelegate next)
+        {
+            _logger = logger;
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                Guid errorId = Guid.NewGuid();
+
+                _logger.LogError(ex,$"{errorId} - {ex.Message}");
+                httpContext.Response.StatusCode = (int)httpContext.Response.StatusCode;
+                httpContext.Response.ContentType = "application/json";
+                var error = new
+                {
+                    Id  = errorId,
+                    Message = "Internal Server Error. We'll fix it soon. :)",
+
+                };
+                await httpContext.Response.WriteAsJsonAsync(error);
+            }
+        }
+    }
+}
